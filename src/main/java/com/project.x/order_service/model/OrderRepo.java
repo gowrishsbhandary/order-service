add08e7cafbc259496c9aec44ef68516f5fa2ecb
gowrishsbhandary <gowrishsbhandary@gmail.com>
2025-01-31 12:44:33 +0200

    Order service using redis

---
+++ src/main/java/com/project.x/order_service/model/OrderRepo.java
@@ -0,0 +1,8 @@
+package com.project.x.order_service.model;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepo extends MongoRepository<Order, String> {
+}
